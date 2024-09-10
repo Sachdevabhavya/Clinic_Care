008d9042e0eee80a4e9a3f0d92181e63db5f210b
sachdevabhavya <sach.bhavya@gmail.com>
2024-09-10 19:20:36 +0530

    jwt authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken")
+const {user_secret, doctor_secret} = require("../config/jwtconfig")
+
+const user_auth_middleware = async(req,res,next) => {
+    var token = req.params.token
+
+    if(!token) {
+        return res.status(404).json({message : "Access denied , no token provided"})
+    }
+
+    try {
+        console.log("token",token)
+        const decoded = jwt.verify(token, user_secret)
+        req.user = decoded
+        console.log("decoded",decoded)
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const doctor_auth_middleware = async(req,res,next) => {
+    var token = req.params.token
+
+    if(!token) {
+        return res.status(404).json({message : "Access denied , no token provided"})
+    }
+
+    try {
+        console.log("token",token)
+        const decoded = jwt.verify(token, doctor_secret)
+        req.user = decoded
+        console.log("decoded",decoded)
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    user_auth_middleware,
+    doctor_auth_middleware
+}
