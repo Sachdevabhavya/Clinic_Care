217db1a42e4ab6e595a48dbee509d30fb6ee40e2
sachdevabhavya <sach.bhavya@gmail.com>
2024-09-10 19:27:51 +0530

    appointment control function routing

--- routes/appointment_booking_routes.js
+++ routes/appointment_booking_routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const appointment_control = require("../controllers/appointment-booking-controller");
-
+const {user_auth_middleware} = require("../middleware/auth")
 const appointment_router = express.Router();
 
 appointment_router.get("/doctor_login/:loginId/appointments", appointment_control.getAllAppointments);
 appointment_router.get("/doctor_login/:loginId/appointments/:appointmentId", appointment_control.getAppointmentById);
-appointment_router.post("/patient_login/:loginId/create_appointment", appointment_control.createAppointment);
+appointment_router.post("/patient_login/:token/create_appointment", user_auth_middleware , appointment_control.createAppointment);
 
 module.exports = appointment_router;
