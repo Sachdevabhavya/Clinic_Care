359a20a4c05893698eb303d62358bf0afdbd4395
sachdevabhavya <sach.bhavya@gmail.com>
2024-09-10 19:28:05 +0530

    lab control function routing

--- routes/lab_routes.js
+++ routes/lab_routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const lab_control = require("../controllers/create-lab-results");
-
+const {doctor_auth_middleware} = require("../middleware/auth")
 const lab_router = express.Router();
 
-lab_router.post("/doctor_login/:loginId/create_lab_records", lab_control.sendrecords);
+lab_router.post("/doctor_login/:token/create_lab_records",doctor_auth_middleware, lab_control.sendrecords);
 
 
 module.exports = lab_router;
