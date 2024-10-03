25834651b043aa3d353fb45585ac1339974d2e8e
sachdevabhavya <sach.bhavya@gmail.com>
2024-10-03 20:06:27 +0530

    jwt config

--- config/jwtconfig.js
+++ config/jwtconfig.js
@@ -2,5 +2,6 @@ require("dotenv").config()
 
 module.exports = {
     user_secret : process.env.USER_JWT_SECRET_KEY,
-    doctor_secret : process.env.DOCTOR_JWT_SECRET_KEY
+    doctor_secret : process.env.DOCTOR_JWT_SECRET_KEY,
+    reset_secret : process.env.PASSWORD_RESET_SECRET_KEY
 }
