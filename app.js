30d47af4c136297b2f1314bc9ff5508997ad6be0
sachdevabhavya <sach.bhavya@gmail.com>
2024-10-20 17:24:56 +0530

    backend server main

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const jwt = require('jsonwebtoken')
+const path = require("path")
+const doctors_model = require("./model/doctor_login")
 // const multer = require("multer"); 
+const {user_secret , doctor_secret} = require("./config/jwtconfig")
 
 const user_router = require("./routes/user_login_routes")
 const doctor_router = require("./routes/doctor_login_routes")
@@ -13,19 +17,81 @@ const validation_email = require("./routes/validate_email_routes")
 const forgot_password = require("./routes/patient_forgot_pass")
 const laboratory_test = require("./routes/lab_test_routes")
 const lab_test_appointment = require("./routes/lab_test_booking_routes")
+const patients_list = require("./routes/list_of_patients_route")
 // const order_router = require("./routes/order_medicine_routes");
 
 const app = express();
 require("dotenv").config();
+app.use('/user_images', express.static(path.join(__dirname, 'user_images')));
+app.use('/doctor_images', express.static(path.join(__dirname, 'doctor_images')));
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 
-app.use((req, res, next) => {      
-    res.set('Cache-Control', 'no-store')
-    next()
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/main' , (req ,res) => {
+    res.render('main')
 })
 
+app.get('/main/doctor_login/:token/doctor_dashboard', (req, res) => {
+    const token = req.params.token;
+    
+    jwt.verify(token, doctor_secret, (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(403).send("Unauthorized access"); 
+        }
+        
+        const user = {
+            name: decoded.name,
+            email: decoded.email,
+            phone_no: decoded.phone_no
+        };
+        
+        res.render('doctor_dashboard', {token , obj: user });
+    });
+});
+
+
+app.get('/main/patient_login/:token/dashboard', (req, res) => {
+    const token = req.params.token;
+    
+    jwt.verify(token, user_secret, (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(403).send("Unauthorized access"); 
+        }
+        
+        const user = {
+            name: decoded.name,
+            email: decoded.email,
+            phone_no: decoded.phone_no
+        };
+        
+        res.render('patient_dashboard', {token , obj: user });
+    });
+});
+
+
+app.get('/main/patient_login/:token/lab_tests' , async(req,res) => {
+    const token = req.params.token
+    const doctors = await doctors_model.find()
+    res.render('list_of_lab_doc' , {token , doctors})
+})
+
+// app.get("/main/patient_login/:token/doctors" , async(req,res) => {
+//     const token = req.params.token
+//     const doctors = await doctors_model.find()
+//     res.render('list_of_doctors' , {token , doctors})
+// })
+
+// app.get("/main/patient_login/:token/:doctor_id/create_appointment" , (req,res) => {
+    
+//     res.render('book_appointment' )
+// })
+// 6713b5298704580fd7cc99e3
 app.use('/main', user_router)
 app.use('/main', doctor_router)
 app.use('/main', appointment_router)
@@ -37,6 +103,7 @@ app.use('/main',validation_email)
 app.use('/main',forgot_password)
 app.use('/main',laboratory_test)
 app.use('/main',lab_test_appointment)
+app.use('/main',patients_list)
 // app.use('/api', order_router);
 
 const PORT = 4000;
